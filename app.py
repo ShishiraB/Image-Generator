9bc8922db4bcad2eafcd5a0a8696f33846ae9d1f
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-03-04 11:22:37 +0530

    App update

--- app.py
+++ app.py
@@ -15,6 +15,16 @@ app.title("Image Generator")
 ctk.set_appearance_mode("light")
 text = ctk.CTkEntry(app, height=40, width=512, font=("Roboto", 20), text_color="black", fg_color="white")
 text.place(x=10, y=10)
+
+#Image Generation
+def gen():
+    with autocast(dev):
+        image = pipe(text.get(), guidance_scale=8.5)["sample"][0]
+    image.save('output.png')
+    img = ImageTk.PhotoImage(image)
+    imain.configure(image=img)
+
+#Button
 trig = ctk.CTkButton(app, height=40, width=120,font=("Roboto", 20), text_color="white", fg_color="orange", command=gen)
 trig.configure(text="Create")
 trig.place(x=206, y=60)
@@ -25,13 +35,5 @@ model = "CompVis/stable-diffusion-v1-4"
 dev = "cpu"
 pipe = StableDiffusionPipeline.from_pretrained(model, revision=None, torch_dtype=torch.float64, use_auth_token=auth_token)
 pipe.to(dev)
-
-#Image Generation
-def gen():
-    with autocast(dev):
-        image = pipe(text.get(), guidance_scale=8.5)["sample"][0]
-    image.save('output.png')
-    img = ImageTk.PhotoImage(image)
-    imain.configure(image=img)
 
 app.mainloop()
