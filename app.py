9b1dac22815e5c72332008492aec8bd50fcc6695
Shishira Bhat <shishira.21ai050@sode-edu.in>
2023-03-04 10:49:42 +0530

    Update

--- app.py
+++ app.py
@@ -12,12 +12,15 @@ from diffusers import StableDiffusionPipeline
 app = tk.Tk()
 app.geometry("532x632")
 app.title("Image Generator")
-
 ctk.set_appearance_mode("light")
 text = ctk.CTkEntry(app, height=40, width=512, font=("Roboto", 20), text_color="black", fg_color="white")
 text.place(x=10, y=10)
+trig = ctk.CTkButton(app, height=40, width=120,font=("Roboto", 20), text_color="white", fg_color="orange", command=gen)
+trig.configure(text="Create")
+trig.place(x=206, y=60)
 imain = ctk.CTkLabel(app, height=512, width=512)
 imain.place(x=10, y=100)
+
 model = "CompVis/stable-diffusion-v1-4"
 dev = "cpu"
 pipe = StableDiffusionPipeline.from_pretrained(model, revision=None, torch_dtype=torch.float64, use_auth_token=auth_token)
@@ -25,12 +28,9 @@ pipe.to(dev)
 
 def gen():
     with autocast(dev):
-        image = pipe(text.get(), guidance_scale=8.5)["Sample"][0]
+        image = pipe(text.get(), guidance_scale=8.5)["sample"][0]
     image.save('output.png')
     img = ImageTk.PhotoImage(image)
     imain.configure(image=img)
-trig = ctk.CTkButton(app, height=40, width=120,font=("Roboto", 20), text_color="white", fg_color="orange", command=gen)
-trig.configure(text="Create")
-trig.place(x=206, y=60)
 
 app.mainloop()
